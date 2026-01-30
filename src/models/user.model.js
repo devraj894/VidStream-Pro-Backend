9528f78f722d31d37c50ee8fcd43343ff4eb417d
Devraj Songara <devrajsongara894@gmail.com>
2026-01-30 19:16:26 +0530

    Feat: implement register user logic and fix bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from  "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     username: {
@@ -44,10 +44,9 @@ const userSchema = new Schema({
     }
 }, {timestamps: true})
 
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function(){
     if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
