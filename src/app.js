bccce7f909e28f390bb6baff685c6777883d2817
Devraj Songara <devrajsongara894@gmail.com>
2026-02-24 15:10:45 +0530

    feat: setup routes for dashboard and tweets management endpoints

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ import videoRouter from "./routes/video.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import commentRouter from  "./routes/comment.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 
 //routes declaration
@@ -28,5 +30,7 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export { app }
