9144124e5c101644ad01767a07d63d2cf6d0b0ef
Devraj Songara <devrajsongara894@gmail.com>
2026-02-28 14:49:52 +0530

    feat: implement endpoint to check server health

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import commentRouter from  "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
@@ -33,5 +34,6 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export { app }
