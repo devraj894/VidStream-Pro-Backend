0fcb465daaadb70f6beb4b7ba8b84b963038e64f
Devraj Songara <devrajsongara894@gmail.com>
2026-05-22 15:58:48 +0530

    feat: include video description in channel videos response

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -101,6 +101,7 @@ const getChannelVideos = asyncHandler(async (req, res) => {
             $project: {
                 thumbnail: 1,
                 title: 1,
+                description: 1,
                 duration: 1,
                 views: 1,
                 likes: 1,
